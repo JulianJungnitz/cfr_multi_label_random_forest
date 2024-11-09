468b3feda5216be8fdbb9dca7856158cf3bf7f8e
Julian Jungnitz <julian@MacBook-Pro.local>
2024-11-09 18:31:39 +0100

    changed split method

--- app/random_forest.py
+++ app/random_forest.py
@@ -48,29 +48,15 @@ def get_all_binaries():
 
 def get_full_data_set(limit=None):
     query = """
-    MATCH (bs:Biological_sample)
+    MATCH (bs:Biological_sample)-[:HAS_DISEASE]->(d:Disease)
     Optional MATCH (bs)-[:HAS_PHENOTYPE]->(ph:Phenotype)
     Optional MATCH (bs)-[:HAS_DAMAGE]->(g:Gene)
-    Optional MATCH (bs)-[:HAS_DISEASE]->(d:Disease)
     RETURN ID(bs) as subject_id, collect(distinct ph.id) AS phenotypes, collect(distinct g.id) AS genes, collect(distinct d.id) as diseases, collect(distinct d.name) AS disease_names
     """
     if limit:
         query += f" LIMIT {limit}"
     data = execute_query(query)
     return data
-
-
-def remove_control(df):
-    control = df[df["disease_names"].apply(lambda x: "control" in x)]
-    if control.empty:
-        logger.error("No control in the database")
-    for index, row in control.iterrows():
-        diseases = row["disease_names"]
-        logger.info(row["subject_id"])
-        if len(diseases) > 1:
-            logger.error(f"Control has more than one disease: {diseases}")
-        df = df.drop(index)
-    return df, control
 
 
 def filter_with_binarizer(df, pheno_binarizer, gene_binarizer):
@@ -90,13 +76,21 @@ def filter_with_binarizer(df, pheno_binarizer, gene_binarizer):
 
 def duplicate_columns_with_multiple_diseases(df):
     df_exploded = df.explode(["diseases", "disease_names"]).reset_index(drop=True)
-    logger.info(f"DataFrame before exploding: {len(df)} rows. Dataframe after exploded: {len(df_exploded)} rows after exploding.")
+    logger.info(
+        f"DataFrame before exploding: {len(df)} rows. Dataframe after exploded: {len(df_exploded)} rows after exploding."
+    )
     return df_exploded
 
 
 def get_features_and_labels(df):
     features = df.drop(columns=["subject_id", "diseases", "disease_names"])
-    labels = df["disease_names"].apply(lambda x: 0 if (isinstance(x, list) and len(x) == 0) or ((not isinstance(x, list)) and pd.isna(x)) else 1)
+    labels = df["disease_names"].apply(
+        lambda x: (
+            0
+            if (isinstance(x, list) and len(x) == 1 and x[0] == "control")
+            else 1
+        )
+    )
     return features, labels
 
 
@@ -108,12 +102,12 @@ def train_classifier(features, labels):
     return clf
 
 
-def split_train_test(df, split_by_control):
-    if split_by_control:
-        df, control = remove_control(df)
-    else:
-        df, control = train_test_split(df, test_size=0.2, random_state=1)
+def split_train_test(
+    df,
+):
+    df, control = train_test_split(df, test_size=0.2, random_state=1)
     return df, control
+
 
 def predict_on_control(features, clf):
     y_pred = clf.predict(features)
@@ -125,12 +119,15 @@ def save_to_csv(subject_ids, disease, filename):
     df = pd.DataFrame({"subject_id": subject_ids, "disease": disease})
     df.to_csv(filename, index=False)
 
-def transform_data_into_features_and_labels(data, pheno_binarizer, gene_binarizer, explode_diseases):
+
+def transform_data_into_features_and_labels(
+    data, pheno_binarizer, gene_binarizer,
+):
     df_final = filter_with_binarizer(data, pheno_binarizer, gene_binarizer)
-    if explode_diseases:
-        df_final = duplicate_columns_with_multiple_diseases(df_final)
+    # df_final = duplicate_columns_with_multiple_diseases(df_final)
     features, labels = get_features_and_labels(df_final)
     return features, labels
+
 
 def main():
     global logger
@@ -141,10 +138,6 @@ def main():
     driver = utils.connect_to_neo4j()
 
     config = utils.read_config()
-    split_type = config.get("SPLIT_TYPE", "random")
-    split_by_control = split_type== "control"
-    explode_diseases = config.get("EXPLODE_DISEASES", True)
-    logger.info(f"Split type: {split_by_control}")
 
     pheno_binarizer, gene_binarizer = get_all_binaries()
 
@@ -153,22 +146,28 @@ def main():
 
     # logger.info(f"Dataframe values of diseases: {df['disease_names']}")
 
-    df, control = split_train_test(df,split_by_control)
+    df, control = split_train_test(df)
 
-    features, labels = transform_data_into_features_and_labels(df, pheno_binarizer, gene_binarizer, explode_diseases)
+    features, labels = transform_data_into_features_and_labels(
+        df, pheno_binarizer, gene_binarizer, 
+    )
 
     clf = train_classifier(features, labels)
-    
-    control_features, control_labels = transform_data_into_features_and_labels(control, pheno_binarizer, gene_binarizer, False)
 
+    control_features, control_labels = transform_data_into_features_and_labels(
+        control, pheno_binarizer, gene_binarizer
+    )
 
     y_pred = predict_on_control(control_features, clf)
 
     save_to_csv(control["subject_id"], y_pred, "./predictions.csv")
 
-    logger.info(f"Shapes: {features.shape}, {labels.shape} ")
+    logger.info("----------------------------------------")
+    logger.info(f"Training Shapes: {features.shape}, {labels.shape} ")
     logger.info(f"Shapes Control: {control_features.shape}, {control_labels.shape} ")
-    logger.info(f"Accuracy: {metrics.f1_score(control_labels, y_pred)}")
+    logger.info(f"{metrics.classification_report(control_labels, y_pred)}")
+
+    
 
 
     # error werfen wenn control mit anderer disease in der db ist
